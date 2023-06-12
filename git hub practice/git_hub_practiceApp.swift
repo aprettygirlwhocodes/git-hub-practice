a760499a5b63caf716e5025ab975a1d2194f00f1
leona <leonafrancis14@gmail.com>
2023-06-12 11:21:10 -0400

    Initial Commit

---
+++ git hub practice/git_hub_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_hub_practiceApp.swift
+//  git hub practice
+//
+//  Created by scholar on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct git_hub_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
